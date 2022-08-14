0cb85ebaf6cf6db19320b77cc6c06f6cbe051f2e
Joey Cadieux <instructor.joeycadieux@gmail.com>
2022-08-14 18:53:44 -0400

    inm.

--- apps/lib/main.dart
+++ apps/lib/main.dart
@@ -1,20 +1,11 @@
 import 'package:flutter/material.dart';
 
-class Main extends StatefulWidget {
-  const Main({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-  @override
-  _MainState createState() => _MainState();
-}
-
-class _MainState extends State<Main> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-      title: Text('MobileNet | OSS Network'),
-      backgroundColor: Colors.blue,
-    )));
+        home: Scaffold(appBar: AppBar(title: Text('MobileNet | OSS Network'))));
   }
 }
