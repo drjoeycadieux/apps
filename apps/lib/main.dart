be8beacd3ece877787b769ab18b987f5c4a7e3f0
Joey Cadieux <instructor.joeycadieux@gmail.com>
2022-08-14 18:44:21 -0400

    inm.

--- apps/lib/main.dart
+++ apps/lib/main.dart
@@ -1,38 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+class Main extends StatefulWidget {
+  const Main({Key? key}) : super(key: key);
 
-class MyApp extends StatelessWidget {
+  @override
+  _MainState createState() => _MainState();
+}
+
+class _MainState extends State<Main> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(),
         home: Scaffold(
             appBar: AppBar(
-              backgroundColor: Colors.black,
-              title: Text('MobileNet | OSS Network'),
-              centerTitle: true,
-              actions: <Widget>[
-                IconButton(
-                  icon: const Icon(Icons.notifications),
-                  onPressed: () {},
-                )
-              ],
-            ),
-            body: Center(child: Text('More Technical Topic Coming Soon.'))));
-  }
-}
-
-class Page1 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return const Center(
-      child: AboutListTile(
-        icon: Icon(Icons.info),
-        aboutBoxChildren: [
-          Text('Tech Center', style: TextStyle(color: Colors.blue))
-        ],
-      ),
-    );
+      title: Text('MobileNet | OSS Network'),
+      backgroundColor: Colors.blue,
+    )));
   }
 }
