42b1d60d0b809ca6ffeb3e77ee2ddd78d4263672
Joey Cadieux <instructor.joeycadieux@gmail.com>
2022-08-14 14:05:38 -0400

    inc.

--- apps/lib/main.dart
+++ apps/lib/main.dart
@@ -6,9 +6,33 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(primaryColor: Colors.blueGrey),
+        theme: ThemeData(),
         home: Scaffold(
-            appBar: AppBar(title: Text('MobileNet | OSS Network')),
+            appBar: AppBar(
+              backgroundColor: Colors.black,
+              title: Text('MobileNet | OSS Network'),
+              centerTitle: true,
+              actions: <Widget>[
+                IconButton(
+                  icon: const Icon(Icons.notifications),
+                  onPressed: () {},
+                )
+              ],
+            ),
             body: Center(child: Text('More Technical Topic Coming Soon.'))));
+  }
+}
+
+class Page1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: AboutListTile(
+        icon: Icon(Icons.info),
+        aboutBoxChildren: [
+          Text('Tech Center', style: TextStyle(color: Colors.blue))
+        ],
+      ),
+    );
   }
 }
